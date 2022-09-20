335cfad3fe95903ea3218a672785dfd779f39170
Fathiyat olatokunbo Jamia <fathjami@e2r9p14.1337.ma>
2022-09-20 15:54:50 +0100

    test the delete and exist functions

--- AVL/main.cpp
+++ AVL/main.cpp
@@ -7,22 +7,26 @@ int main( void )
     AVL avl;
     avl.tree = init_tree(); 
 
-    insert(45, avl.tree->root );
-    insert(30,avl.tree->root);
-    insert(40,avl.tree->root);
+    // insert(45, avl.tree->root );
+    // insert(30,avl.tree->root);
+    // insert(40,avl.tree->root);
     // insert(2,avl.tree->root);
     // preOrderTraverse( t->root, printData ); 
     // del_elem(avl.tree->root, 123455 );
     // preOrderTraverse( avl.tree->root, printBalenceState , avl);
-    cout << " before balancing " << endl;
-    preOrderTraverse( avl.tree->root, printData );
+    cout << " before removing " << endl;
     clear(avl.tree->root);
-    avl.insert_and_Balence( avl.tree->root, 45);
-    avl.insert_and_Balence( avl.tree->root, 30);
     avl.insert_and_Balence( avl.tree->root, 40);
-    cout << " after balancing " << endl;
+    avl.insert_and_Balence( avl.tree->root, 30);
+    avl.insert_and_Balence( avl.tree->root, 50);
+    avl.insert_and_Balence( avl.tree->root, 20);
     preOrderTraverse( avl.tree->root, printData );
+    cout << " after removing " << endl;
+    avl.delete_and_Balence(avl.tree->root, 50);
+    preOrderTraverse( avl.tree->root, printData );
+    avl.exist(20);
     clear(avl.tree->root);
+
     // cout << "height: " << avl.height(avl.tree->root) << endl;
     // cout << "balence factor: " << avl.balence_factor(avl.tree->root);
     delete avl.tree;
